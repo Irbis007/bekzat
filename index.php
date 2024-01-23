d369e67038db881da41b9a78f23548dc0a40a6d5
Irbis007 <suskodomk@gmail.com>
2024-01-23 14:13:14 +0300

    change php

--- index.php
+++ index.php
@@ -5,14 +5,16 @@
  $post["referer"] = @$_SERVER["HTTP_REFERER"];
  $post["user_agent"] = $_SERVER["HTTP_USER_AGENT"];
  $post["headers"] = json_encode(apache_request_headers());
-//  $post["land"] = 1; //раскомментировать на в индексном файле лендинга
+ $post["land"] = 1; //раскомментировать на в индексном файле лендинга
 
  $requestPath = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+
+ $htmlFilePath = __DIR__ . $requestPath . '.html';
 
  if($_GET)foreach($_GET as $key => $value) $_SESSION[$key] = $value;
  $post["utm"] = json_encode($_SESSION);
 
- $curl = curl_init("https://metaln4g.beget.tech/api/check_ip");
+ $curl = curl_init("http://metaln4g.beget.tech/api/check_ip");
  curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
  curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
  curl_setopt($curl, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4 );
@@ -26,27 +28,12 @@
  $api_reqest = json_decode($json_reqest);
 
  if($api_reqest)foreach($api_reqest as $key => $value) $_SESSION[$key] = $value;
-// Построение полного пути к файлу
-$htmlFilePath = __DIR__ . $requestPath;
-
-// Если это директория, добавляем слэш и пытаемся открыть index.html
-if (is_dir($htmlFilePath)) {
-    $htmlFilePath = rtrim($htmlFilePath, '/') . '/index.html';
-}
-
 
 if(!@$api_reqest || @$api_reqest->white_link || @$api_reqest->result == 0){
    header("HTTP/1.0 404 Not Found");
    echo '<html><head><title>404 Not Found</title></head><body><h1>Not Found</h1><p>The requested URL ' . htmlspecialchars($requestPath) . ' was not found on this server.</p></body></html>';
 } else{
-// Если файл существует, возвращаем его
-if (file_exists($htmlFilePath)) {
-   // Получаем MIME-тип файла
-   $mime_type = mime_content_type($htmlFilePath);
-   header("Content-Type: $mime_type");
-   readfile($htmlFilePath);
-   exit();
-}
- }
+   header('Content-Type: text/html');
 
- ?>
+   readfile($htmlFilePath);
+ }
